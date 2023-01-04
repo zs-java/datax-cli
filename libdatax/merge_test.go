28cb0837ef1ef040d8ff76e16ef72ed0f32d5b7a
朱帅 <zhushuai_it@163.com>
2023-01-04 16:39:32 +0800

    commit.

--- libdatax/merge_test.go
+++ libdatax/merge_test.go
@@ -6,7 +6,6 @@ import (
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
-	"reflect"
 	"testing"
 )
 
@@ -73,87 +72,10 @@ func saveJson(data interface{}, path string) error {
 	return encoder.Encode(data)
 }
 
-func JsonMerge(dst, src map[string]interface{}) map[string]interface{} {
-	return jsMerge(dst, src, 0)
-}
-
-func jsMerge(dst, src map[string]interface{}, depth int) map[string]interface{} {
-	jsonMergeDepth := 32
-	if depth > jsonMergeDepth {
-		return dst
-		// panic("too deep!")
-	}
-
-	for key, srcVal := range src {
-
-		if dstVal, ok := dst[key]; ok {
-
-			srcMap, srcMapOk := jsMapify(srcVal)
-			dstMap, dstMapOk := jsMapify(dstVal)
-
-			if srcMapOk && dstMapOk {
-				srcVal = jsMerge(dstMap, srcMap, depth+1)
-			} else {
-				srcMapArr, srcMapArrOk := parseMapArray(srcVal)
-				dstMapArr, dstMapArrOk := parseMapArray(dstVal)
-				if srcMapArrOk && dstMapArrOk && len(srcMapArr) == len(dstMapArr) {
-					arr := make([]map[string]interface{}, len(srcMapArr))
-					for i := 0; i < len(srcMapArr); i++ {
-						arr[i] = jsMerge(srcMapArr[i], dstMapArr[i], depth+1)
-					}
-					srcVal = arr
-				}
-			}
-		}
-
-		dst[key] = srcVal
-	}
-
-	return dst
-}
-
-func jsMapify(i interface{}) (map[string]interface{}, bool) {
-
-	value := reflect.ValueOf(i)
-
-	if value.Kind() == reflect.Map {
-
-		m := map[string]interface{}{}
-
-		for _, k := range value.MapKeys() {
-			m[k.String()] = value.MapIndex(k).Interface()
-		}
-
-		return m, true
-	}
-
-	return map[string]interface{}{}, false
-}
-
 func TestArray(t *testing.T) {
 	data := readJson("arr.json")
 	array, ok := parseMapArray(data)
 	fmt.Println(ok, array)
-}
-
-func parseMapArray(data interface{}) ([]map[string]interface{}, bool) {
-	value := reflect.ValueOf(data)
-
-	var arr []map[string]interface{}
-	switch reflect.TypeOf(data).Kind() {
-	case reflect.Slice, reflect.Array:
-		if value.Len() == 0 {
-			return nil, false
-		}
-		for i := 0; i < value.Len(); i++ {
-			mapify, isMap := jsMapify(value.Index(i).Interface())
-			if !isMap {
-				return nil, false
-			}
-			arr = append(arr, mapify)
-		}
-	}
-	return arr, true
 }
 
 func TestDir(t *testing.T) {
